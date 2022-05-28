12821fd0d5f19694a792d8d00909c228bcf270e7
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-05-28 20:16:28 +0800

    enhance structure

--- 2022_5_27/src/App.java
+++ 2022_5_27/src/App.java
@@ -14,17 +14,57 @@ import java.util.PriorityQueue;
  * @Package : PACKAGE_NAME
  * @createTime : 2022/5/27 10:34
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @github : <a href="https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30">...</a>
  * @Description :
  */
+
+
+class IntCmp implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
+    }
+}
+
+class Student implements Comparable<Student> {
+    int age;
+
+    public Student(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.age - o.age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                '}';
+    }
+}
+
 public class App {
 
-    public static void main4(String[] args) {
+    public static void main(String[] args) {
+        PriorityQueue<Student> priorityQueue = new PriorityQueue<>();
+        priorityQueue.offer(new Student(12));
+        priorityQueue.offer(new Student(10));
+        priorityQueue.offer(new Student(9));
+        System.out.println(priorityQueue.toString());
 
     }
 
+
     public static void main3(String[] args) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        IntCmp intCmp = new IntCmp();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(intCmp);
+        priorityQueue.offer(1);
+        priorityQueue.offer(2);
+        priorityQueue.offer(3);
+        System.out.println(priorityQueue.toString());
 
     }
 
@@ -38,7 +78,7 @@ public class App {
     }
 
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Integer[] array = new Integer[]{1, 3, 5, 6, 6, 7, 8, 9, 10, 12};
         Comparator<? super Integer> comparator = new Comparator<Integer>() {
             @Override
