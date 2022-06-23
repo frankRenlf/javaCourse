15c64f272229726edb43ad5d68f7919d97e75e54
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-23 15:10:01 +0800

    optimize test

--- 2022_5_30/src/App.java
+++ 2022_5_30/src/App.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
 public class App {
 
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 //        Arrays.sort();
         TestSorts testSorts = new TestSorts();
         testSorts.testInsertSort();
@@ -31,7 +31,7 @@ public class App {
 
     }
 
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         int[] arr = new int[]{3,19, 1, 3, 1, 4, 5, 2, 0, 7, 4, 5};
         Sorts sorts = new Sorts();
         sorts.countSort(arr);
