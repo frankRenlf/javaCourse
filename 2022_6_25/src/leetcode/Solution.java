bdf68ba100cedaab55afe1bd7f2d213a4e7aa330
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-30 20:37:47 +0800

    implement topKFrequent2 method with PriorityQueue

--- 2022_6_25/src/leetcode/Solution.java
+++ 2022_6_25/src/leetcode/Solution.java
@@ -29,6 +29,31 @@ class Node {
 
 public class Solution {
 
+    public List<String> topKFrequent2(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                return entry1.getValue() == entry2.getValue() ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        List<String> ret = new ArrayList<String>();
+        while (!pq.isEmpty()) {
+            ret.add(pq.poll().getKey());
+        }
+        Collections.reverse(ret);
+        return ret;
+    }
+
+
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> map = new HashMap<>();
         for (String s : words) {
