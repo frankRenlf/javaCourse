45a107917c47e10db140274a9ee3bc9ee28afc16
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-02 14:36:56 +0800

    complete hash buck

--- 2022_7_2/src/Test.java
+++ 2022_7_2/src/Test.java
@@ -1,3 +1,7 @@
+import hash.HashBuck;
+
+import java.util.Objects;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -11,11 +15,61 @@
  * @Description :
  */
 
+class Person {
+    public int id;
+    public String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
 
 public class Test {
 
     public static void main(String[] args) {
+        HashBuck<Person, Integer> hashBuck = new HashBuck<>();
+        hashBuck.put(new Person(1, "a"), 11);
+        hashBuck.put(new Person(2, "c"), 22);
+        Person p1 = new Person(3, "d");
+        hashBuck.put(p1, 33);
+        hashBuck.put(new Person(4, "e"), 44);
+        hashBuck.put(new Person(44,"f"),44);
+        hashBuck.put(new Person(12, "f"), 1212);
+        System.out.println(hashBuck.get(p1));
 
+    }
+
+    public static void main1(String[] args) {
+        HashBuck<Integer, String> hashBuck = new HashBuck<>();
+        hashBuck.put(1, "a");
+        hashBuck.put(2, "b");
+        hashBuck.put(0, "c");
+        hashBuck.put(5, "d");
+        hashBuck.put(10, "e");
+        hashBuck.put(12, "f");
+        System.out.println(hashBuck.get(12));
     }
 
 }
--- 2022_7_2/src/hash/HashBuck.java
+++ 2022_7_2/src/hash/HashBuck.java
@@ -12,35 +12,86 @@ package hash;
  * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
  * @Description :
  */
-public class HashBuck {
+public class HashBuck<K, V> {
 
-    static class Node {
-        int key;
-        int val;
-        Node next;
+    static class Node<K, V> {
+        K key;
+        V val;
+        Node<K, V> next;
 
-        public Node(int key, int val) {
+        public Node(K key, V val) {
             this.key = key;
             this.val = val;
             this.next = null;
         }
     }
 
-    private Node[] array;
+    private Node<K, V>[] array;
     private int usedSize;
+    private static final int DEFAULT_SIZE = 5;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private static final int DEFAULT_EXTEND_COEFFICIENT = 1;
 
     public HashBuck() {
-        array = new Node[10];
+        array = new Node[DEFAULT_SIZE];
     }
 
     // add element
-    public void put(int key, int val) {
+    public void put(K key, V val) {
+        Node<K, V> node = new Node<>(key, val);
+        int index = key.hashCode() % array.length;
+        Node<K, V> cur = array[index];
+        while (cur != null) {
+            if (cur.key.equals(key)) {
+                cur.val = val;
+                return;
+            }
+            cur = cur.next;
+        }
+        node.next = array[index];
+        array[index] = node;
+        usedSize++;
+
+        if (overLoadFactor()) {
+            resize();
+        }
 
     }
 
-    // get element
-    public void get(int key) {
+    private void resize() {
+        Node<K, V>[] tmp = new Node[array.length + DEFAULT_EXTEND_COEFFICIENT * DEFAULT_SIZE];
+        for (int i = 0; i < array.length; i++) {
+            Node<K, V> cur = array[i];
+            while (cur != null) {
+                int index = cur.key.hashCode() % tmp.length;
+                Node<K, V> curNext = cur.next;
+                cur.next = tmp[index];
+                tmp[index] = cur;
+                cur = curNext;
+            }
+        }
+        array = tmp;
+    }
 
+    private boolean overLoadFactor() {
+        return loadFactor() >= DEFAULT_LOAD_FACTOR;
+    }
+
+    private double loadFactor() {
+        return usedSize * 1.0 / array.length;
+    }
+
+    // get element
+    public V get(K key) {
+        int index = key.hashCode() % array.length;
+        Node<K, V> cur = array[index];
+        while (cur != null) {
+            if (cur.key.equals(key)) {
+                return cur.val;
+            }
+            cur = cur.next;
+        }
+        return null;
     }
 
 }
