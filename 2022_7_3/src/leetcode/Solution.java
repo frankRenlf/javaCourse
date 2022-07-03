cf3fc0c36fb6298c53a3e20bb10be96d919c8e7d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-03 12:13:06 +0800

    add daily work method

--- 2022_7_3/src/leetcode/Solution.java
+++ 2022_7_3/src/leetcode/Solution.java
@@ -13,4 +13,12 @@ package leetcode;
  * @Description :
  */
 public class Solution {
+
+    public int singleNumber(int[] nums) {
+        int ret = 0;
+        for (int x : nums) {
+            ret ^= x;
+        }
+        return ret;
+    }
 }
